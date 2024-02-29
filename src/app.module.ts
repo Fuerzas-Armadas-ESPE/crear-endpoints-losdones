817717e0fd6261599437c912e16483d098b8df80
josuealeman17 <eljossam@hotmail.com>
2024-02-29 18:10:07 -0500

    adding comments to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comment.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
+    MongooseModule.forRoot('mongodb://localhost:27017/my_database'),
     PostsModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
